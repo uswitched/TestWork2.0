02e1e68767c19ab6dcc4ce26a97253d5bf52804f
uswitch <usflua@gmail.com>
2021-03-27 20:38:35 +0200

    SplashScreenActivity.kt

---
+++ app/src/main/java/ua/uswitch/testwork20/MainActivity.kt
@@ -0,0 +1,6 @@
+package ua.uswitch.testwork20
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+}
---
+++ app/src/main/java/ua/uswitch/testwork20/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package ua.uswitch.testwork20
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+        }, 2500)
+    }
+
+}
